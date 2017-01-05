c12a97cb84d9afc403f152439b62f622d55ec3f6
herve bassinot <herve.bassinot@savoirfairelinux.com>
2017-01-05 14:08:22 -0500

    some works

--- js/standaloneDB.js
+++ js/standaloneDB.js
@@ -2,7 +2,6 @@
 var debug_mode = true;
 var debug_msg = "";
 var debug_selector = "#debug_db";
-var account;
 
 var TYPE_DEBIT = "DEBIT";
 var TYPE_CREDIT = "CREDIT";
@@ -33,66 +32,77 @@ function createAccount(acc_name, acc_description, acc_amount) {
     });
 }
 
-// retourne le compte qui correspond à l'id et 
-function getAccount(acc_id) {
+// retourne le compte qui correspond à l'id et
+function addOperation(ope_date, ope_description, ope_type, ope_amount, acc_id) {
     db.transaction(function (tx) {
         tx.executeSql('SELECT * FROM emove_accounts WHERE acc_id = ' + acc_id, [], function (tx, results) {
             var len = results.rows.length, i;
-            
+
             if(len != 0) {
-                account = results.rows.item(0);
                 debug_msg = "<p>Account found: " + acc_id + "</p>";
+                account = results.rows.item(0);
+                checkOperation(tx, ope_date, ope_description, ope_type, ope_amount, account);
+
             } else {
-                account = null;
-                debug_msg = "<p>Account not found: " + acc_id + "</p>";
+                debug_msg = "<p>Invalid operation. Account not found: " + acc_id + "</p>";
             }
 
-            if(debug_mode) { 
+            if(debug_mode) {
                 document.querySelector(debug_selector).innerHTML += debug_msg;
             }
         }, null);
     });
 }
 
+function checkOperation(tx, ope_date, ope_description, ope_type, ope_amount, account) {
+  tx.executeSql('SELECT * FROM emove_operations WHERE acc_id = ' + account.acc_id + ' and ope_type = "'+ ope_type + '" and ope_description = "' + ope_description + '" and ope_date = "' + ope_date + '" and ope_amount = ' + ope_amount, [], function (tx, results) {
+    var len = results.rows.length, i;
+    if(len == 0) {
+      createOperation(tx, ope_date, ope_description, ope_type, ope_amount, account);
+      debug_msg = "<p>Valid Operation.</p>";
+    } else {
+      debug_msg = "<p>Invalid operation. Already exist</p>";
+    }
+
+    if(debug_mode) {
+        document.querySelector(debug_selector).innerHTML += debug_msg;
+    }
+  }, null);
+}
+
 // Credite le compte du montant passé en parametre
-function creditAccount(tx, acc_id, amount) {
+function creditAccount(tx, account, amount) {
     a = account.acc_amount + amount;
-    tx.executeSql('UPDATE emove_accounts SET acc_amount='+a+' WHERE acc_id='+acc_id);
+    tx.executeSql('UPDATE emove_accounts SET acc_amount='+a+' WHERE acc_id='+account.acc_id);
     return a;
 }
 
 // Debite le compte
-function debitAccount(tx, acc_id, amount) {
+function debitAccount(tx, account, amount) {
     a = account.acc_amount - amount;
-    tx.executeSql('UPDATE emove_accounts SET acc_amount='+a+' WHERE acc_id='+acc_id);
+    tx.executeSql('UPDATE emove_accounts SET acc_amount='+a+' WHERE acc_id='+account.acc_id);
     return a;
 }
 
 // Creation d'une nouvelle operation
-function createOperation(ope_date, ope_description, ope_type, ope_amount, acc_id) {
-    getAccount(acc_id);
-    db.transaction(function (tx) {
+function createOperation(tx, ope_date, ope_description, ope_type, ope_amount, account) {
+      if(TYPE_DEBIT == ope_type) {
+          ope_amount_after = debitAccount(tx, account, ope_amount);
+      }
+      else if (TYPE_CREDIT == ope_type) {
+          ope_amount_after = creditAccount(tx, account, ope_amount);
+      }
+      else {
+          return;
+      }
+      ope_number = 1;
 
-    if(account) {
-        if(TYPE_DEBIT == ope_type) {
-            ope_amount_after = debitAccount(tx, acc_id, ope_amount);
-        }
-        else if (TYPE_CREDIT == ope_type) {
-            ope_amount_after = creditAccount(tx, acc_id, ope_amount);
-        }
-        else {
-            return;
-        }
-        ope_number = 1;
+      tx.executeSql('INSERT INTO emove_operations (ope_description, ope_type, ope_date, ope_number, ope_amount, ope_amount_before, ope_amount_after, acc_id) VALUES ("'+ope_description+'", "'+ope_type+'", "'+ope_date+'", '+ope_number+', '+ope_amount+', '+account.acc_amount+', '+ope_amount_after+', '+account.acc_id+')');
 
-        tx.executeSql('INSERT INTO emove_operations (ope_description, ope_type, ope_date, ope_number, ope_amount, ope_amount_before, ope_amount_after, acc_id) VALUES ("'+ope_description+'", "'+ope_type+'", "'+ope_date+'", '+ope_number+', '+ope_amount+', '+account.acc_amount+', '+ope_amount_after+', '+acc_id+')');
-
-        if(debug_mode) {
-            debug_msg = '<p>insert new operation into table.</p>';
-            document.querySelector(debug_selector).innerHTML += debug_msg;
-        }
-    }
-    });
+      if(debug_mode) {
+          debug_msg = '<p>insert new operation into table.</p>';
+          document.querySelector(debug_selector).innerHTML += debug_msg;
+      }
 }
 
 
